9d787a177eb517e19eea5d411fd124541943a1b1
ldarki <danilotnahuel2@gmail.com>
2025-10-17 13:22:50 -0300

    chore: format

--- src/utils/helpers.rs
+++ src/utils/helpers.rs
@@ -18,13 +18,27 @@ pub fn humanize_time_since(time: DateTime<Local>) -> String {
     }
 }
 
-/// Converts a byte count into a human-readable string, such as "1 B", "1.5 KB", or "1.23 MB".
+/// Converts a byte count into a human-readable string.
+///
+/// The function takes a byte count as input and returns a string describing the byte count in a human-readable format, such as "1 B", "1.5 KB", or "1.23 MB".
 pub fn humanize_bytes(bytes: u64) -> String {
-    if bytes < 1024 {
+    const KILOBYTE: u64 = 1024;
+    const MEGABYTE: u64 = KILOBYTE * 1024;
+    const GIGABYTE: u64 = MEGABYTE * 1024;
+    const TERABYTE: u64 = GIGABYTE * 1024;
+    const PETABYTE: u64 = TERABYTE * 1024;
+
+    if bytes < KILOBYTE {
         format!("{} B", bytes)
-    } else if bytes < 1024 * 1024 {
-        format!("{:.1} KB", bytes as f64 / 1024.0)
+    } else if bytes < MEGABYTE {
+        format!("{:.1} KB", (bytes as f64) / KILOBYTE as f64)
+    } else if bytes < GIGABYTE {
+        format!("{:.2} MB", (bytes as f64) / MEGABYTE as f64)
+    } else if bytes < TERABYTE {
+        format!("{:.2} GB", (bytes as f64) / GIGABYTE as f64)
+    } else if bytes < PETABYTE {
+        format!("{:.2} TB", (bytes as f64) / TERABYTE as f64)
     } else {
-        format!("{:.2} MB", bytes as f64 / (1024.0 * 1024.0))
+        format!("{:.2} PB", (bytes as f64) / PETABYTE as f64)
     }
 }
